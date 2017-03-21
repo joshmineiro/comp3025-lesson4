d5cfb2727045317a439f6e272f2bd53055933f23
joshmineiro <joshmineiro@gmail.com>
2017-03-21 17:32:36 -0400

    Updated game to show text

--- Scripts/Core/game.js
+++ Scripts/Core/game.js
@@ -1,6 +1,31 @@
 /* core game script */
 //IIFE - Immediately Invoked Function Expression
 (function () {
-    console.log("Game Started");
+    //CreateJS variables
+    var canvas;
+    var stage;
+    //Game variables
+    var helloLabel;
+    function Start() {
+        canvas = document.getElementById("canvas"); //get reference to canvas element
+        stage = new createjs.Stage(canvas); //creating new stage object
+        createjs.Ticker.framerate = 60; //set fps to 60
+        createjs.Ticker.on("tick", Update); //calls the update function every frame
+        Game();
+    }
+    function Update(event) {
+        helloLabel.rotation += 5; //rotate clockwise 5 degrees every frame
+        stage.update(); // manually refreshes/redraws stage
+    }
+    function Game() {
+        console.log("Game started...");
+        helloLabel = new createjs.Text("Hello world!", "40px Consolas", "#000000");
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+        helloLabel.x = 160;
+        helloLabel.y = 240;
+        stage.addChild(helloLabel);
+    }
+    window.onload = Start; //calls the start function when the window finishes loading
 })();
 //# sourceMappingURL=game.js.map
--- Scripts/Core/game.ts
+++ Scripts/Core/game.ts
@@ -3,6 +3,40 @@
 //IIFE - Immediately Invoked Function Expression
 
 (function(){
-console.log("Game Started");
+//CreateJS variables
+let canvas: HTMLElement;
+let stage: createjs.Stage;
+
+//Game variables
+let helloLabel: createjs.Text;
+
+function Start(): void{
+    canvas = document.getElementById("canvas"); //get reference to canvas element
+    stage = new createjs.Stage(canvas); //creating new stage object
+    createjs.Ticker.framerate = 60; //set fps to 60
+    createjs.Ticker.on("tick", Update); //calls the update function every frame
+
+    Game();
+}
+
+function Update(event:createjs.Event):void{
+
+    helloLabel.rotation += 5; //rotate clockwise 5 degrees every frame
+
+    stage.update();// manually refreshes/redraws stage
+}
+
+function Game():void{
+    console.log("Game started...")
+
+    helloLabel = new createjs.Text("Hello world!", "40px Consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    helloLabel.x = 160;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
+}
+
+window.onload = Start; //calls the start function when the window finishes loading
 
 })();
